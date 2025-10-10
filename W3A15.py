f4d3d42c806fa874e075e69fd2d8c23046743691
Nguyễn Việt Dũng <lavalloyal@gmail.com>
2025-10-10 13:59:01 +0700

    Bài W3A15

---
+++ W3A15.py
@@ -0,0 +1,11 @@
+a = float(input('Nhập điểm của bạn: '))
+if a<0:
+    print('Lỗi')
+elif a >= 8:
+    print('Giỏi')
+elif a >= 6.5:
+    print('Khá')
+elif a >= 5:
+    print('Trung bình')
+elif a < 5:
+    print('Yếu')
